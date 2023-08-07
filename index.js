92bf54e2268004f71df6a67934fc192c154b906b
JuanitoDeLaTorre <home.john.thomas@gmail.com>
2023-08-07 10:20:38 -0600

    google cloud work

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const app = express();
 const cors = require("cors");
 const multer = require("multer");
 
+//GOOGLE CLOUD SETUP
+const { Storage } = require("@google-cloud/storage");
+const gc = new Storage({
+  keyFilename: path.join(__dirname, "/uppytesting-04f85dc78b7f.json"),
+  projectId: "uppytesting",
+});
+
+const uppyBucket = gc.bucket("uppytest-2296");
+
+gc.getBuckets().then((x) => console.log(x));
+
+///////////////////
+
 const port = 3030;
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -30,14 +43,46 @@ app.get("/bundle.js", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "bundle.js"));
 });
 
-app.post("/image", upload, (req, res) => {
+app.post("/image", upload, async (req, res) => {
   console.log(req.file);
-  console.log(req.body.imageData);
+  // console.log(req.body.imageData);
+  // const imageData = req.body.imageData;
+  // const imageBuffer = Buffer.from(imageData, "base64");
 
-  const imageBuffer = Buffer.from(imageData, "base64");
+  // console.log(imageBuffer);
 
-  console.log(imageBuffer);
+  const uploadedFile = await uploadToGoogleCloudStorage(req.file);
+  console.log("File uploaded to Google Cloud Storage:", uploadedFile);
 });
+
+async function uploadToGoogleCloudStorage(file) {
+  return new Promise((resolve, reject) => {
+    // Set the destination file name in Google Cloud Storage
+    // const fileName = `${Date.now()}_${path.basename(file.originalname)}`;
+    const fileName = "testerfile";
+
+    // Upload the file to the specified bucket
+    const bucket = uppyBucket;
+    const fileStream = bucket.file(fileName).createWriteStream({
+      metadata: {
+        contentType: file.mimetype,
+      },
+    });
+
+    fileStream.on("error", (err) => {
+      reject(err);
+    });
+
+    fileStream.on("finish", () => {
+      // Set the public URL for the file
+      const publicUrl = `https://storage.googleapis.com/uppytest-2296/${fileName}`;
+      resolve(publicUrl);
+    });
+
+    // Pipe the file data into the write stream
+    fileStream.end(file.buffer);
+  });
+}
 
 app.listen(port, (req, res) => {
   console.log("Listening on port " + port);
